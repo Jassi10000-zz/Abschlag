d88c3add8613f1ede3b956c3f2b72694c4c306c3
Jaskeerat Singh <jaskeerat10000@gmail.com>
2021-07-29 18:45:22 +0530

    up with setting up database and other essential stuff , finished the project

--- models/article.js
+++ models/article.js
@@ -1,21 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const marked = require('marked')
+const slugify = require('slugify')
+const createDomPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window)
+
 const articleSchema = new mongoose.Schema({
-    title: {
-        required: true,
-        type: String   
-    },
-    desc: {
-        required: true,
-        type: String
-    },
-    markdown: {
-        required: true,
-        type: String
-    },
-    date: {
-        default: Date.now,
-        type: Date
-    }
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  markdown: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  sanitizedHtml: {
+    type: String,
+    required: true
+  }
 })
 
-module.exports = mongoose.model("Article" , articleSchema);
+articleSchema.pre('validate', function(next) {
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true })
+  }
+
+  if (this.markdown) {
+    this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
+  }
+
+  next()
+})
+
+module.exports = mongoose.model('Article', articleSchema)
--- server.js
+++ server.js
@@ -1,35 +1,25 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const articleRouter = require("./routes/articles");
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
 
-// connecting to database to save the articles
-mongoose.connect("mongodb://localhost/blog" , {
-    useNewUrlParser: true  ,  useUnifiedTopology: true 
-});
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
 
-app.set("view engine" , 'ejs');
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-app.use(express.urlencoded({urlencoded: false}));
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
 
-app.get("/" , (req,res) => {
-    const articles = [
-        {
-            title: "First article",
-            date: new Date(),
-            desc: " Desc 1 "
-        },
-        {
-            title: "Second article",
-            date: new Date(),
-            desc: " Desc 2 "
-        },
-        ]
-    res.render("articles/index" , {articles: articles});
-});
-
-app.use("/articles" , articleRouter);
+app.use('/articles', articleRouter)
 
 app.listen(8000 , () => {
-    console.log("server running at port 8000");
+    console.log("Server running at 8000");
 });
